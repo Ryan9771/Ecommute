0f32135dbbcf19f4d41f12eebf4c1938f8e72410
ryan <ryan.patel21@imperial.ac.uk>
2023-02-05 00:08:09 +0000

    got grid ppattern for big screen

--- properties/property_api.py
+++ properties/property_api.py
@@ -1,71 +1,71 @@
-import requests
-import json
+# import requests
+# import json
 
 
-class Address:
-    def __init__(self, country, state, city):
-        self.country = country
-        self.state = state
-        self.city = city
+# class Address:
+#     def __init__(self, country, state, city):
+#         self.country = country
+#         self.state = state
+#         self.city = city
 
 
-class Property:
-    def __init__(self, num_bathrooms, num_bedrooms, img_src, price, address):
-        self.num_bathrooms = num_bathrooms
-        self.num_bedrooms = num_bedrooms
-        self.img_src = img_src
-        self.price = price
-        self.address = address
+# class Property:
+#     def __init__(self, num_bathrooms, num_bedrooms, img_src, price, address):
+#         self.num_bathrooms = num_bathrooms
+#         self.num_bedrooms = num_bedrooms
+#         self.img_src = img_src
+#         self.price = price
+#         self.address = address
 
 
-class PropertyAPI:
-    """
-    Class to abstract API requests for property info.
-    """
-    def __init__(self, price, num_bedrooms, address):
-        self.price = price
-        self.num_bedrooms = num_bedrooms
-        self.address = address
-        self.headers = {
-        	"X-RapidAPI-Key": "99d643f4cemshd9cede2b5ccd5bep1ddab9jsne1d397ebcee0",
-        	"X-RapidAPI-Host": "zillow56.p.rapidapi.com"
-        }
-        self.url = "https://zillow56.p.rapidapi.com/search"
+# class PropertyAPI:
+#     """
+#     Class to abstract API requests for property info.
+#     """
+#     def __init__(self, price, num_bedrooms, address):
+#         self.price = price
+#         self.num_bedrooms = num_bedrooms
+#         self.address = address
+#         self.headers = {
+#         	"X-RapidAPI-Key": "99d643f4cemshd9cede2b5ccd5bep1ddab9jsne1d397ebcee0",
+#         	"X-RapidAPI-Host": "zillow56.p.rapidapi.com"
+#         }
+#         self.url = "https://zillow56.p.rapidapi.com/search"
 
-    def call_req(self):
-        querystring = {
-            "location": self.address.city + ", " + self.address.state, 
-            "status":"forRent", 
-            "hasAirConditioning":"true"
-        }
-        response = requests.request("GET", self.url, headers=self.headers, params=querystring)
-        property_list = []
-        for p in json.loads(response.text)["results"]:
-            property_list.append(
-                Property(
-                    p["bathrooms"], 
-                    p["bedrooms"], 
-                    p["imgSrc"], 
-                    p["price"], 
-                    Address(
-                        p["country"],
-                        p["state"],
-                        p["city"]
-                    )
-                )
-            )
+#     def call_req(self):
+#         querystring = {
+#             "location": self.address.city + ", " + self.address.state, 
+#             "status":"forRent", 
+#             "hasAirConditioning":"true"
+#         }
+#         response = requests.request("GET", self.url, headers=self.headers, params=querystring)
+#         property_list = []
+#         for p in json.loads(response.text)["results"]:
+#             property_list.append(
+#                 Property(
+#                     p["bathrooms"], 
+#                     p["bedrooms"], 
+#                     p["imgSrc"], 
+#                     p["price"], 
+#                     Address(
+#                         p["country"],
+#                         p["state"],
+#                         p["city"]
+#                     )
+#                 )
+#             )
         
-        return property_list
+#         return property_list
 
 
-if __name__ == "__main__":
-    price = 1000
-    num_bedrooms = 2
-    address = Address("USA", "tx", "san francisco")
-    property = PropertyAPI(price, num_bedrooms, address)
-    property_list = property.call_req()
-    for p in property_list:
-        print(f"bedrooms: {p.num_bedrooms}, bathrooms: {p.num_bathrooms}, img_src: {p.img_src}, price: {p.price}, address: {p.address.country}, {p.address.state}, {p.address.city}")
+# if __name__ == "__main__":
+#     price = 1000
+#     num_bedrooms = 2
+#     address = Address("USA", "tx", "san francisco")
+#     property = PropertyAPI(price, num_bedrooms, address)
+#     property_list = property.call_req()
+#     for p in property_list:
+#         print(f"bedrooms: {p.num_bedrooms}, bathrooms: {p.num_bathrooms}, img_src: {p.img_src}, price: {p.price}, address: {p.address.country}, {p.address.state}, {p.address.city}")
 
     
 
--- properties/views.py
+++ properties/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-import property_api
+# import property_api
 
 # Create your views here.
 # Takes a request and returns a response (called request handler)
 
 def get_properties(request):
-    price = int(request.GET.get('price', 250000))
-    location = Address('USA', request.GET.get('state', 'ny'), request.GET.get('city', 'new york'))
-    num_bedrooms = request.GET.get('num_bedrooms', 1)
+    # price = int(request.GET.get('price', 250000))
+    # location = Address('USA', request.GET.get('state', 'ny'), request.GET.get('city', 'new york'))
+    # num_bedrooms = request.GET.get('num_bedrooms', 1)
     
-    query = Property(price, num_bedrooms, location)
-    properties = query.call_req()
+    # query = Property(price, num_bedrooms, location)
+    # properties = query.call_req()
+    return render(request, 'index.html')
